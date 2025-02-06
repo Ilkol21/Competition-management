9b03d931669f49f593efd30e70da74c50bac3c7f
illia <illya.kolom01@gmail.com>
2025-02-07 01:10:29 +0200

    Finish

--- results/src/main/java/com/example/results/Result.java
+++ results/src/main/java/com/example/results/Result.java
@@ -1,11 +1,14 @@
 package com.example.results;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Result {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private Long competitionId;
   private Long participantId;
